3ec1febda1d15777749acb6ccfd09a9383300235
Sobh6n <sobhankhedry7297@gmail.com>
2025-03-23 16:09:36 +0330

    changed the messages and updated the model and database

--- SoftWare_Engineering/Program.cs
+++ SoftWare_Engineering/Program.cs
@@ -2,8 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using SoftWare_Engineering.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("https://0.0.0.0:7006", "http://0.0.0.0:7007");
 
 // Add services to the container.
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins"; //added 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,7 +14,16 @@ builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(connectionString));
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+
 var app = builder.Build();
+app.UseCors("AllowAll");//added
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,8 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+//app.UseHttpsRedirection();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
